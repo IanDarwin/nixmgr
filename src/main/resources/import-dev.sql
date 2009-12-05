2cc4d3513ce8a75a1378bca02968d41457ebc3a6
ian <ian@darwinsys.com>
2009-12-05 23:57:17 +0000

    typo

--- src/main/resources/import-dev.sql
+++ src/main/resources/import-dev.sql
@@ -7,5 +7,5 @@ insert into account(id,firstname,lastname,unixcreated,username,password) values(
 
 -- user in roles
 insert into user_in_role(id,account,role) values(0,0,0);
-insert into user_in_role(id,account,role) values(0,0,5);
+insert into user_in_role(id,account,role) values(1,0,5);
 
