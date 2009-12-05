fc56e4193739a9919ec8fa0ef35b59e7712d72e3
ian <ian@darwinsys.com>
2009-12-05 20:27:09 +0000

    Sync with database.

--- src/main/resources/import-dev.sql
+++ src/main/resources/import-dev.sql
@@ -3,9 +3,9 @@ insert into userrole(id,name) values(0, 'admin');
 insert into userrole(id,name) values(5, 'student');
 
 -- account
-insert into account(id,firstname,lastname,unixcreated,username,password) values(0,'Ian','Darwin',f,'ian','fakePW');
+insert into account(id,firstname,lastname,unixcreated,username,password) values(0,'Ian','Darwin','true','ian','fakePW');
 
 -- user in roles
-insert into user_in_role(id,account,userrole) values(0,0,0);
-insert into user_in_role(id,account,userrole) values(0,0,5);
+insert into user_in_role(id,account,role) values(0,0,0);
+insert into user_in_role(id,account,role) values(0,0,5);
 
