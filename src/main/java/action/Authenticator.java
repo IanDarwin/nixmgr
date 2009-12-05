d0c24979eb3dd2c2cb4bb23d0d70b454927f6fe0
ian <ian@darwinsys.com>
2009-12-05 20:37:53 +0000

    Put back Seam authenticator, apparently deleted by accident.

---
+++ src/main/java/action/Authenticator.java
@@ -0,0 +1,45 @@
+package action;
+
+import javax.persistence.EntityManager;
+
+import model.Account;
+import model.Userrole;
+
+import org.jboss.seam.annotations.In;
+import org.jboss.seam.annotations.Logger;
+import org.jboss.seam.annotations.Name;
+import org.jboss.seam.log.Log;
+import org.jboss.seam.security.Credentials;
+import org.jboss.seam.security.Identity;
+
+@Name("authenticator")
+public class Authenticator {
+	@Logger
+	private Log log;
+	@In
+	EntityManager entityManager;
+	@In
+	Identity identity;
+	@In
+	Credentials credentials;
+
+	public boolean authenticate() {
+		log.info("authenticating {0}", credentials.getUsername());
+		Account p = null;
+		try {
+			p = (Account) entityManager.createQuery("from Account p where p.login = #{credentials.username}").getSingleResult();
+		} catch (Exception e) {
+			System.err.println("FAIL: " + e);
+			return false;
+		}
+		if (!p.getPassword().equals(credentials.getPassword())) {
+			System.err.println("FAIL: password");
+			return false;
+		}
+		for (Userrole r : p.getRoles()) {
+			identity.addRole(r.getName());
+		}
+		return true;
+	}
+}
+
