9fde51444742841cd584c814334dfbf03805c6cb
ian <ian@darwinsys.com>
2009-12-06 03:32:57 +0000

    Add chuma

--- src/main/resources/import-dev.sql
+++ src/main/resources/import-dev.sql
@@ -4,6 +4,7 @@ insert into userrole(id,name) values(5, 'student');
 
 -- account
 insert into account(id,firstname,lastname,unixcreated,username,password) values(0,'Ian','Darwin','true','ian','fakePW');
+insert into account(id,firstname,lastname,unixcreated,username,password) values(1,'Chuma','Chukwulozie','true','chuma','fakePW');
 
 -- user in roles
 insert into user_in_role(id,account,role) values(0,0,0);
