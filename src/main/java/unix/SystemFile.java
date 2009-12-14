5b11ed3db31c2f7994eeb7783cbfc3535dc6a8b2
ian <ian@darwinsys.com>
2009-12-14 23:11:19 +0000

    Change accessor name from getUnixName to getName.

--- src/main/java/unix/SystemFile.java
+++ src/main/java/unix/SystemFile.java
@@ -12,7 +12,9 @@ public enum SystemFile {
 		this.unixName = unixName;
 	}
 
-	public String getUnixName() {
+	public String getName() {
+		// If you ever need another OS, expand the constructor args, and
+		// switch on the current OS here. For now:
 		return unixName;
 	}
 }
