08a9e5646e4d78f160e560b0b4a3d49c12d13ef9
ian <ian@darwinsys.com>
2009-12-14 22:52:36 +0000

    Written but not tested

--- src/main/java/unix/SystemFileAccessor.java
+++ src/main/java/unix/SystemFileAccessor.java
@@ -1,13 +1,53 @@
 package unix;
 
-import java.util.List;
+import java.io.*;
+import java.util.*;
 
 public class SystemFileAccessor {
-	public List<String> load(SystemFile f) {
-		return null;
+
+	public static List<String> load(SystemFile sf) {
+
+		String fileName = sf.getName();
+		File f = new File(fileName);
+		BufferedReader is = null;
+		try {
+			is = new BufferedReader(new FileReader(f));
+			List<String> ret = new ArrayList<String>();
+			String line;
+			while ((line = is.readLine()) != null) {
+				ret.add(line);
+			}
+			return ret;
+		} catch (IOException e) {
+			throw new RuntimeException("I/O Error on file " + fileName, e);
+		} finally {
+			if (is != null) {
+				try {
+					is.close();
+				} catch (IOException e2) {
+					System.out.println("ERROR IN CLOSE: " + e2);
+				}
+			}
+		}
 	}
 	
-	public void store(SystemFile f, List<String> contents) {
+	public static void store(SystemFile sf, List<String> contents) {
 		
+		String fileName = sf.getName();
+		File f = new File(fileName);
+		PrintWriter p = null;
+		try {
+			p = new PrintWriter(new FileWriter(f));
+			for (String line : contents) {
+				p.println(line);
+			}
+			return;
+		} catch (IOException e) {
+			throw new RuntimeException("I/O Error on file " + fileName, e);
+		} finally {
+			if (p != null) {
+				p.close();
+			}
+		}
 	}
 }
