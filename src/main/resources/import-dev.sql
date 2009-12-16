6ddb5d939ed22987615f2f5388b72bfb58ccec87
ian <ian@darwinsys.com>
2009-12-16 23:41:06 +0000

    Get viewing of user's own print balance working. Set non-zero print balance for me for manual testing of viewing.

--- src/main/resources/import-dev.sql
+++ src/main/resources/import-dev.sql
@@ -3,7 +3,7 @@ insert into userrole(id,name) values(0, 'admin');
 insert into userrole(id,name) values(5, 'student');
 
 -- account
-insert into account(id,firstname,lastname,email,unixcreated,username,password) values(0,'Ian','Darwin','ian@darwinsys.com','true','ian','fakePW');
+insert into account(id,firstname,lastname,email,unixcreated,username,password,printcredits) values(0,'Ian','Darwin','ian@darwinsys.com','true','ian','fakePW',77);
 insert into account(id,firstname,lastname,email,unixcreated,username,password) values(1,'Chuma','Chukwulozie','chuma@ernescliff.ca','true','chuma','fakePW');
 
 -- user in roles
