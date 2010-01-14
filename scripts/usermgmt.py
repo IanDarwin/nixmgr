6145581caa1bc681d17b4eb6a76287d792c000a0
ian <ian@darwinsys.com>
2010-01-14 15:06:26 +0000

    Format error messages for CUPS

--- scripts/usermgmt.py
+++ scripts/usermgmt.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python
 
 ''' CUPS filter for accounting for SNMP-managed networked printers,
-	parts based loosely on accsnmp,
+	learned how to do some of these things from the excellent (but Perl-based)
 	see http://fritz.potsdam.edu/projects/cupsapps/#accsnmp.
-	Uses our account database instead of text files.
+	My version uses our account database instead of text files.
 '''
 
 # TODO
 # exec the real backend
-# print status messagess correctly
 
 # later: get SNMP actually working XXX for now daily*.py does the actual billing!
 
@@ -21,10 +20,10 @@ from ChargeForPages import ChargeForPages
 
 prefix="usermgmt"		# used in device URL
 
-# To find out if your printer supportss  the needed MIB:
+# To find out if your printer supports the needed MIB:
 # snmpget -v 1 -c 'public' ianprint 1.3.6.1.2.1.43.10.2.1.4.1.1
-LIFEPAGECOUNT_OID = "1.3.6.1.2.1.43.10.2.1.4.1.1";	#printer lifetime pagecount
-PRINTERSTATUS_OID = "1.3.6.1.2.1.25.3.5.1.1.1";		#printer status
+LIFEPAGECOUNT_OID = "1.3.6.1.2.1.43.10.2.1.4.1.1";	# pages since printer manufactured
+PRINTERSTATUS_OID = "1.3.6.1.2.1.25.3.5.1.1.1";		# current printer status
 
 cupsBackendDir = "/usr/local/libexec/cups/backend"	# Somewhat OS-specific
 
@@ -34,7 +33,7 @@ def validateUser(userName):
 	creditBal = accountant.getCurrentPageCredits(userName)
 	print "Current credit for %s is %d" % (userName, creditBal)
 	if creditBal < 1:
-		print "ERROR: User %s has page credit of %d" % (userName, creditBal)
+		sys.stderr.write("ERROR: User %s has page credit of %d" % (userName, creditBal))
 		sys.exit(1)
 
 def printerJobPages():
@@ -59,7 +58,7 @@ def	main():
 		print("network %s \"Unknown\" \"Accounted Printer (SNMP)\"" % prefix)
 		sys.exit(0)
 	if nargs < 5 or nargs > 6:
-		print("Usage: %s jobid username jobtitle copies printopts [file]" % sys.argv[0])
+		sys.stderr.write("ERROR: Usage: %s jobid username jobtitle copies printopts [file]" % sys.argv[0])
 		sys.exit(1)
 
 	if nargs == 5:
@@ -75,13 +74,12 @@ def	main():
 	try:
 		devURI = os.environ["DEVICE_URI"];
 	except KeyError:
-		print "No DEVICE_URI in environment!"
-		# sys.exit(1)
-		devURI = prefix + "://foo/printer1"
+		sys.stderr.write("ERROR: No DEVICE_URI in environment!")
+		sys.exit(1)
 
 	m = re.match(r'%s://(.+)/(.*)'%prefix, devURI);
 	if m == None:
-		print "ERROR: Could not parse DEVICE_URI"
+		sys.stderr.write("ERROR: Could not parse DEVICE_URI")
 		sys.exit(1)
 	global realBackEnd
 	realBackEnd = m.group(1)
@@ -99,8 +97,6 @@ def	main():
 	n2 = printerJobPages()
 
 	billUser(userName, n2 - n1)
-
-	print "All done"
 
 	sys.exit(0)
 
