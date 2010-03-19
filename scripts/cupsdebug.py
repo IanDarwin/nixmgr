ac2c6f6fdc9a71a770b502c5bdd282030af88f34
ian <ian@darwinsys.com>
2010-03-19 00:36:53 +0000

    minor repairs

--- scripts/cupsdebug.py
+++ scripts/cupsdebug.py
@@ -4,32 +4,28 @@ import os,re,sys,time
 from syslog import openlog,syslog
 prefix = "cups-debug"
 
-def	main():
-	openlog("cups-debug")
-	syslog("Hello from cups-debug backend")
-	args = sys.argv[1:] # minus progname
-	nargs = len(args)
-	if nargs < 5 or nargs > 6:
-		sys.stderr.write("ERROR: Usage: %s jobid username jobtitle copies printopts [file]\n" % sys.argv[0])
-		sys.exit(1)
-	if nargs == 5:
-		(jobId,userName,jobTitle,copies,printOpts) = args
-		fileName = "sys.stdin"
-	if nargs == 6:
-		(jobId,userName,jobTitle,copies,printOpts,fileName) = args
+openlog("cups-debug")
+syslog("Hello from cups-debug backend")
+args = sys.argv[1:] # minus progname
+nargs = len(args)
+if nargs < 5 or nargs > 6:
+	sys.stderr.write("ERROR: Usage: %s jobid username jobtitle copies printopts [file]\n" % sys.argv[0])
+	sys.exit(1)
+if nargs == 5:
+	(jobId,userName,jobTitle,copies,printOpts) = args
+	fileName = "sys.stdin"
+if nargs == 6:
+	(jobId,userName,jobTitle,copies,printOpts,fileName) = args
 
-	# Grab the device id from the environment, make sure it's for us, strip
-	# off our prefix, and return the result to the environment.
+# Grab the device id from the environment, make sure it's for us, strip
+# off our prefix, and return the result to the environment.
 
-	try:
-		devURI = os.environ["DEVICE_URI"]
-	except KeyError:
-		devURI = "ERROR: No DEVICE_URI in environment!"
+try:
+	devURI = os.environ["DEVICE_URI"]
+except KeyError:
+	devURI = "(No env(DEVICE_URI))"
 
-	debug = \
-	    "jobId %s, userName %s, jobTitle %s, copies %d, printOpts %s, devURI %s\n" % jobId, userName, jobTitle, copies, printOpts, fileName
-	sys.stderr.write(debug + "\n")
-	logger(debug)
-
-if __name__ == '__main__':
-	main()
+debug = \
+    "jobId %s, userName %s, jobTitle %s, copies %s, printOpts %s, filename %s, devURI %s" % (jobId, userName, jobTitle, copies, printOpts, fileName, devURI)
+sys.stderr.write(debug + "\n")
+syslog(debug)
