d5fb52f84bb7078ea45846c98ce40e574f2ca654
ian <ian@darwinsys.com>
2010-06-09 13:07:28 +0000

    Get the remote AUTH_{USERNAME,PASSWORD} if present, log the former and merely note the latter.

--- scripts/cupsdebug.py
+++ scripts/cupsdebug.py
@@ -8,6 +8,10 @@ openlog("cups-debug")
 #syslog("Hello from cups-debug backend")
 args = sys.argv[1:] # minus progname
 nargs = len(args)
+if nargs == 0:
+	# CUPS calls with no args to discover what we do
+	sys.stderr.write("network %s \"Unknown\" \"Accounted Printer (SNMP)\"" % prefix)
+	sys.exit(0)
 if nargs < 5 or nargs > 6:
 	sys.stderr.write("ERROR: Usage: %s jobid username jobtitle copies printopts [file]\n" % sys.argv[0])
 	sys.exit(1)
@@ -16,6 +20,16 @@ if nargs == 5:
 	fileName = "sys.stdin"
 if nargs == 6:
 	(jobId,userName,jobTitle,copies,printOpts,fileName) = args
+
+# DEBUG
+try:
+	authUserName=os.environ["AUTH_USERNAME"]
+	authPassword=os.environ["AUTH_PASSWORD"]
+	authPassword="(present)" # comment out if syslog won't compromise
+	syslog("cupsdebug %s pw %s\n" % (authUserName, authPassword))
+except KeyError:
+	syslog("cupsdebug: NO AUTH\n");
+	sys.stderr.write("ERROR: no auth data\n")
 
 # Grab the device id from the environment, make sure it's for us, strip
 # off our prefix, and return the result to the environment.
